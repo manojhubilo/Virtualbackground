53c30a21fc331b1916db6e5cc5b1261a4157b007
manoj <manoj@hubilo.com>
2022-02-15 13:44:57 +0530

    selfi segment done

--- src/mediapipe/selfie-segment.js
+++ src/mediapipe/selfie-segment.js
@@ -2,9 +2,11 @@ import { Camera } from '@mediapipe/camera_utils';
 import { SelfieSegmentation } from '@mediapipe/selfie_segmentation';
 import { useCallback, useEffect, useState } from 'react';
 
+
+
 function SelfieSegmentationComp() {
 
-    const [isUseBlur, setUseBlur] = useState(false);
+    const [filterValue, setUseBlur] = useState(1);
 
     const onResults = useCallback((results) => {
         const canvasElement = document.getElementsByClassName('output_canvas')[0];
@@ -12,11 +14,11 @@ function SelfieSegmentationComp() {
         // const isBlur = false;
         canvasCtx.save();
         canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-        canvasCtx.drawImage(results.segmentationMask, 0, 0,
-            canvasElement.width, canvasElement.height);
+
 
         // background solution start
-        if (!isUseBlur) {
+        if (filterValue === "2") {
+            canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
             canvasCtx.globalCompositeOperation = 'source-out';
             var blueprint_background = new Image();
             blueprint_background.src = '/tech-paper.png';
@@ -28,25 +30,30 @@ function SelfieSegmentationComp() {
 
             // Only overwrite missing pixels.
             canvasCtx.globalCompositeOperation = 'destination-atop';
-            canvasCtx.drawImage(
-                results.image, 0, 0, canvasElement.width, canvasElement.height);
-            canvasCtx.restore();
+            canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
         }
         // blur solution   
+        else if (filterValue === "3") {
+            canvasCtx.filter = `blur(1px)`;
+            canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+            canvasCtx.globalCompositeOperation = "source-in";
+            canvasCtx.filter = "none";
+            canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+            canvasCtx.globalCompositeOperation = "destination-over";
+            canvasCtx.filter = `blur(4px)`;
+            canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+        }
+        // normal
         else {
-            canvasCtx.globalCompositeOperation = 'xor';
-            canvasCtx.filter = 'blur(8px)';
+            canvasCtx.globalCompositeOperation = "copy";
+            canvasCtx.filter = "none";
             canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
         }
 
-        // Only overwrite missing pixels.
-        canvasCtx.globalCompositeOperation = 'destination-atop';
-        canvasCtx.filter = "none"
-        canvasCtx.drawImage(
-            results.image, 0, 0, canvasElement.width, canvasElement.height);
         canvasCtx.restore();
-
-    }, [isUseBlur])
+    }, [filterValue])
 
     useEffect(() => {
         const videoElement = document.getElementsByClassName('input_video')[0];
@@ -71,18 +78,30 @@ function SelfieSegmentationComp() {
         });
         camera.start();
 
-        return ()=>{
+        return () => {
             selfieSegmentation.close();
+            camera.stop();
         }
 
     }, [onResults]);
+
+    // useEffect(()=>{
+    //     const canvasElement = document.getElementsByClassName('output_canvas')[0];
+    //     const nextVideoElement = document.getElementById("next-video");
+
+    //     var stream = canvasElement.captureStream(25);
+    //     // Set the source of the <video> element to be the stream from the <canvas>.
+    //     nextVideoElement.srcObject = stream;
+    // },[filterValue])
 
     return (
         <div className='selfie_segmentation_container'>
             <video className="input_video" height="420" width="750"></video>
             <canvas className="output_canvas" height="420px" width="750px" ></canvas>
+            <br />
+            <label>filter value <input type="number" max={3} min={1} value={filterValue} onChange={(event) => { setUseBlur(event.target.value) }} /></label>
             <br/>
-            <label>blur only <input type="checkbox" checked={isUseBlur} onChange={() => { setUseBlur(!isUseBlur) }} /></label>
+            {/* <video id="next-video"/> */}
         </div>
     );
 }
